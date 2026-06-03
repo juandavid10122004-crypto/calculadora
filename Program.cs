7ae44f67d18c6e1ffa1d43a7be3a32b4b0c07642
juandavid10122004-crypto <juandavid10122004@gmail.com>
2026-06-03 15:48:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 10;
+            int suma = a + b;
+            int resta = a - b;
+            int multiplicacion = a * b;
+            int division = a / b;
+            Console.WriteLine("El resultado es:");
+            Console.WriteLine("suma: " + suma);
+            Console.WriteLine("resta: " + resta);
+            Console.WriteLine("multiplicacion: " + multiplicacion);
+            Console.WriteLine("Division: " + division);
+        }
+    }
+}
